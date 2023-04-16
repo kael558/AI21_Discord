95c291fa8f357ef435f904f574193c63d54d37ea
Rahel Gunaratne <rahel.gunaratne@gmail.com>
2023-04-16 17:01:08 -0400

    basic & proposal

---
+++ README.md
@@ -0,0 +1,11 @@
+Useful resources:
+
+discord intent breakdown: https://discordpy.readthedocs.io/en/stable/api.html#discord.Intents
+
+cohere's discord bot: https://github.com/cohere-ai/sandbox-grounded-qa/blob/main/discord_bot.py
+
+discord developer link: https://discord.com/developers/applications/932319489243107358/bot
+
+replit code: https://replit.com/@kael558/AI21Discord#main.py
+
+uptime robot to ping replit code: https://uptimerobot.com/
--- main.py
+++ main.py
@@ -3,83 +3,79 @@ import os
 import ai21
 import discord
 
+
 def generate(prompt: str):
-    response = ai21.Completion.execute(
-        model="j2-grande-instruct",
-        prompt=prompt,
-        numResults=1,
-        maxTokens=100,
-        temperature=0.3,
-        topKReturn=0,
-        topP=0.1,
-        countPenalty={
-            "scale": 0,
-            "applyToNumbers": False,
-            "applyToPunctuations": False,
-            "applyToStopwords": False,
-            "applyToWhitespaces": False,
-            "applyToEmojis": False
-        },
-        frequencyPenalty={
-            "scale": 0,
-            "applyToNumbers": False,
-            "applyToPunctuations": False,
-            "applyToStopwords": False,
-            "applyToWhitespaces": False,
-            "applyToEmojis": False
-        },
-        presencePenalty={
-            "scale": 0,
-            "applyToNumbers": False,
-            "applyToPunctuations": False,
-            "applyToStopwords": False,
-            "applyToWhitespaces": False,
-            "applyToEmojis": False
-        },
-        stopSequences=[]
-    )
+  response = ai21.Completion.execute(model="j2-grande-instruct",
+                                     prompt=prompt,
+                                     numResults=1,
+                                     maxTokens=100,
+                                     temperature=0.3,
+                                     topKReturn=0,
+                                     topP=0.1,
+                                     countPenalty={
+                                       "scale": 0,
+                                       "applyToNumbers": False,
+                                       "applyToPunctuations": False,
+                                       "applyToStopwords": False,
+                                       "applyToWhitespaces": False,
+                                       "applyToEmojis": False
+                                     },
+                                     frequencyPenalty={
+                                       "scale": 0,
+                                       "applyToNumbers": False,
+                                       "applyToPunctuations": False,
+                                       "applyToStopwords": False,
+                                       "applyToWhitespaces": False,
+                                       "applyToEmojis": False
+                                     },
+                                     presencePenalty={
+                                       "scale": 0,
+                                       "applyToNumbers": False,
+                                       "applyToPunctuations": False,
+                                       "applyToStopwords": False,
+                                       "applyToWhitespaces": False,
+                                       "applyToEmojis": False
+                                     },
+                                     stopSequences=[])
 
-    if 'completions' not in response or response['completions'][0]['data']['text'] == '' or \
-            response['completions'][0]['data']['text'].isspace():
-        return None
-
-    return response['completions'][0]['data']['text'].strip()
+  return response["completions"][0]['data']['text'].strip()
 
 
 class Client(discord.Client):
-    async def on_ready(self):
-        print("Logged in as", self.user)
 
-    async def answer(self, message):
-        async with message.channel.typing():
-            response = generate(message.clean_content)
-            response_msg = await message.channel.send(response, reference=message)
-            await response_msg.edit(suppress=True)
-            return
+  async def on_ready(self):
+    print("Logged in as", self.user)
 
-    async def on_message(self, message):
-        # Ignore messages from itself to avoid infinite loops
-        if message.author == self.user:
-            return
+  async def answer(self, message):
+    async with message.channel.typing():
+      response = generate(message.clean_content)
+      response_msg = await message.channel.send(response, reference=message)
+      await response_msg.edit(suppress=True)
+      return
 
-        # Check if the message is a direct message
-        if isinstance(message.channel, discord.channel.DMChannel):
-            await self.answer(message)
+  async def on_message(self, message):
+    # Ignore messages from itself to avoid infinite loops
+    if message.author == self.user:
+      return
 
-    async def on_reaction_add(self, reaction, user):
-        # Check if the user is the bot itself
-        if user != self.user:
-            return
+    # Check if the message is a direct message
+    if isinstance(message.channel, discord.channel.DMChannel):
+      await self.answer(message)
 
-        # Check if the reaction is a single question mark
-        if str(reaction.emoji) == "❓" and reaction.count == 1:
-            await self.answer(reaction.message)
+  async def on_reaction_add(self, reaction, user):
+    # Check if the user is the bot itself
+    if user == self.user:
+      return
+
+    # Check if the reaction is a single question mark
+    if str(reaction.emoji) == "❓" and reaction.count == 1:
+      await self.answer(reaction.message)
 
 
 if __name__ == "__main__":
-    ai21.api_key = os.getenv("AI21_API_KEY")
+  ai21.api_key = os.environ['AI21_API_KEY']
+  intents = discord.Intents.all()
 
-    intents = discord.Intents.all()
-    client = Client(intents=intents)
-    token = os.getenv("DISCORD_TOKEN")
-    client.run(token)
+  client = Client(intents=intents)
+  token = os.environ['DISCORD_TOKEN']
+  client.run(token)
