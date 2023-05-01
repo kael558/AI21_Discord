10bf9e059e0eb3044b807a92b14b239e974693b8
Rahel Gunaratne <rahel.gunaratne@gmail.com>
2023-04-30 22:25:07 -0400

    updated req and gitignore

--- restart_client.sh
+++ restart_client.sh
@@ -6,7 +6,10 @@ pkill -f "python3 $DISCORD_BOT_SCRIPT"
 # Delete the data files
 rm data/*
 
-# Rescrape the file
+# Activate the virtual env
+source venv/bin/activate
+
+# Re-scrape the file
 scrapy crawl ai21_spider -O data/AI21.csv
 
 # Setup the index
